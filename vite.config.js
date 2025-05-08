686c8f2702cb5b77cf3de92168e69f0c66e20afe
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2025-05-08 18:34:29 -0400

    Dependencies
    Added:
    react-pdf
    pdfjs-dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['pdfjs-dist/build/pdf.worker.entry']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),      // tu app principal
+        viewer: resolve(__dirname, 'viewer.html')     // entrada del iframe
+      }
+    }
+  }
 })
