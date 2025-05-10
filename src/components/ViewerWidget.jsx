b6c917ff083087b9c584d1cbd1baf4a38f1b253e
AlexMarambio <65684793+AlexMarambio@users.noreply.github.com>
2025-05-10 16:10:02 -0400

    Update ViewerWidget.jsx

--- src/components/ViewerWidget.jsx
+++ src/components/ViewerWidget.jsx
@@ -5,11 +5,27 @@ export default function ViewerWidget({ config, pdfUrl }) {
   const [numPages, setNumPages] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [audio, setAudio] = useState(null);
+  const [volume, setVolume] = useState(0.5);
+  let isPaused = false;
 
   const pageConfig = config.pages.find(p => p.page === currentPage);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+  }
+  function pauseAudio () {
+    if(audio){
+      if(!isPaused){
+        audio.pause().catch(console.warn);
+        document.removeEventListener('click', playAudio);
+        isPaused = true;
+      }
+      else{
+        audio.play().catch(console.warn);
+        document.removeEventListener('click', playAudio);
+        isPaused = false;
+      }
+    }
   }
 
   useEffect(() => {
@@ -20,6 +36,7 @@ export default function ViewerWidget({ config, pdfUrl }) {
 
     if (pageConfig?.audioUrl) {
       const newAudio = new Audio(pageConfig.audioUrl);
+      newAudio.volume = volume;
       setAudio(newAudio);
 
       // autoplay solo si ya hubo interacción
@@ -52,6 +69,30 @@ export default function ViewerWidget({ config, pdfUrl }) {
         <button onClick={() => setCurrentPage(p => Math.min(p + 1, numPages))} disabled={currentPage >= numPages}>
           Siguiente
         </button>
+      </div>
+
+      <div style={{ marginTop: '1rem' }}>
+        <label>
+          Volumen:
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={(e) => {
+              const newVolume = parseFloat(e.target.value);
+              setVolume(newVolume);
+              if (audio) audio.volume = newVolume;
+            }}
+          />
+          <span>{Math.round(volume*100)}%</span>
+        </label>
+        <span>
+            <button onClick={() => pauseAudio()}>
+              pause/play
+            </button>
+          </span>
       </div>
 
     </div>
