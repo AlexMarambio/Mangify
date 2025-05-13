921dc8e862aa250a452c782c7a892104a53e25c8
deigodd <daiscore@gmail.com>
2025-05-12 23:24:53 -0400

    update adding query params

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -11,13 +11,21 @@ export default function Index() {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="flex space-x-4">
         <button
-          onClick={() => handleNavigation("/editor")}
+          onClick={() =>
+            handleNavigation(
+              "/editor?pdfUrl=/armadosMangify.pdf&configUrl=/testConfig.json"
+            )
+          }
           className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition cursor-pointer"
         >
           Editor
         </button>
         <button
-          onClick={() => handleNavigation("/viewer")}
+          onClick={() =>
+            handleNavigation(
+              "/viewer?pdfUrl=/armadosMangify.pdf&configUrl=/testConfig.json"
+            )
+          }
           className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition cursor-pointer"
         >
           Viewer
