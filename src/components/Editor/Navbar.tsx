26b2ab2f912135c90e168f23d9e7eb87d9debbd6
Aro <samuelpozas7@gmail.com>
2025-05-15 00:02:32 -0400

    Navbar Fixed

--- src/components/Editor/Navbar.tsx
+++ src/components/Editor/Navbar.tsx
@@ -2,34 +2,57 @@ import { useAppContext } from "../../context/AppContext"
 import ModalMenu from "./subComponents/ModalMenu"
 
 const NavBar = () => {
-    const { nodos, setNodos, separador, setSeparador, musica, setMusica } = useAppContext()
-    console.log("nodos:", nodos)
-    console.log("separador:", separador)
-    console.log("musica:", musica)
+    const { nodos, setNodos, separador, setSeparador, musica, setMusica } = useAppContext();
+
     return (
-        <div className='grid grid-cols-16 bg-stone-900 w-screen h-full items-center border-b-4 border-stone-600 gap-x-10'>
-            <div className="col-span-2 flex justify-center" >
-                <ModalMenu/>
+        <div className="flex flex-row bg-stone-900 w-screen h-full items-center border-b-4 border-stone-600 px-6 gap-x-6 justify-between">
+            {/* ModalMenu */}
+            <div className="flex justify-center m-2">
+                <ModalMenu />
             </div>
-            <div className="col-span-2 text-center">
-                <button className="m-2 px-5 py-3 rounded-xl hover:bg-stone-800" onClick={() => {setNodos(!nodos), setMusica(false), setSeparador(false)}}>
-                    <span className='text-3xl text-white font-bold text-center'>Nodos</span>
-                </button>
-            </div>
-            <div className="col-span-4 text-center">
-                <button className="m-2 px-5 py-3 rounded-xl hover:bg-stone-800" onClick={() => {setSeparador(!separador), setNodos(false), setMusica(false)}}>
-                    <span className='text-3xl text-white font-bold'>Separador de viñetas</span>
-                </button>
-            </div>
-            <div className="col-span-2 text-center">
-                <button className="m-2 px-5 py-3 rounded-xl hover:bg-stone-800" onClick={() => {setMusica(!musica), setNodos(false), setSeparador(false)}}>
-                    <span className='text-3xl text-white font-bold'>Música</span>
-                </button>
-            </div>
-            <div className="col-span-6 flex flex-row w-full text-center justify-center">
-                <span className='text-5xl text-white font-bold'>Mangify</span> 
+
+            {/* Botón Nodos */}
+            <button
+                className="m-2 px-10 py-3 rounded-xl hover:bg-stone-800"
+                onClick={() => {
+                setNodos(!nodos);
+                setMusica(false);
+                setSeparador(false);
+                }}
+            >
+                <span className="text-2xl text-white font-bold text-center">Nodos</span>
+            </button>
+
+            {/* Botón Separador */}
+            <button
+                className="m-2 px-5 py-3 rounded-xl hover:bg-stone-800"
+                onClick={() => {
+                setSeparador(!separador);
+                setNodos(false);
+                setMusica(false);
+                }}
+            >
+                <span className="text-2xl text-white font-bold">Separador de viñetas</span>
+            </button>
+
+            {/* Botón Música */}
+            <button
+                className="m-2 px-5 py-3 rounded-xl hover:bg-stone-800"
+                onClick={() => {
+                setMusica(!musica);
+                setNodos(false);
+                setSeparador(false);
+                }}
+            >
+                <span className="text-2xl text-white font-bold">Música</span>
+            </button>
+
+            {/* Título */}
+            <div className="flex-grow text-end m-2 px-5 py-3 ">
+                <span className="text-5xl text-white font-bold">Mangify</span>
             </div>
         </div>
-    )
-}
-export default NavBar
+    );
+};
+
+export default NavBar;
